d56256a873894d2666ea2d7c7138a26d86f5d4c5
Lucas Marques Medeiros <lumarque@c1r13s6.42porto.com>
2023-09-06 17:11:05 +0100

    update include

---
+++ Makefile
@@ -0,0 +1,51 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: lumarque <lumarque@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2023/09/06 13:21:14 by lumarque          #+#    #+#              #
+#    Updated: 2023/09/06 13:26:00 by lumarque         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = so_long
+
+SRC = src/main.c src/utils.c src/create_map.c src/clean_exit.c \
+		src/check_map.c src/graphics.c src/moves.c
+
+OBJS = ${SRC:.c=.o}
+
+CC = cc
+RM = rm -f
+CFLAGS = -Wall -Wextra -Werror -g
+INCLUDE = -I include
+MAKE = make -C
+LIBFT_PATH = libs/libft
+MLX_PATH = libs/mlx
+LIBFT = -L ${LIBFT_PATH} -lft
+MLX	= -L ${MLX_PATH} -lmlx -Ilmlx -lXext -lX11 -lm 
+
+.c.o:
+		${CC} ${CFLAGS} ${INCLUDE} -c $< -o ${<:.c=.o}
+
+$(NAME): ${OBJS}
+		${MAKE} ${LIBFT_PATH}
+		${MAKE} ${MLX_PATH}
+		${CC} ${OBJS} ${LIBFT} ${MLX} -o ${NAME}
+
+all:	${NAME}
+
+clean:
+		${MAKE} ${LIBFT_PATH} clean
+		${MAKE} ${MLX_PATH} clean
+		${RM} ${OBJS}
+
+fclean: clean
+		${MAKE} ${LIBFT_PATH} fclean
+		${RM} ${NAME}
+
+re: fclean all
+
+.PHONY: all clean fclean re
--- include/so_long.h
+++ include/so_long.h
@@ -1,30 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lumarque <lumarque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/02 19:17:47 by lumarque          #+#    #+#             */
-/*   Updated: 2023/09/06 13:37:44 by lumarque         ###   ########.fr       */
+/*   Created: 2023/09/06 13:25:56 by lumarque          #+#    #+#             */
+/*   Updated: 2023/09/06 13:38:41 by lumarque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#ifndef SO_LONG_H
+# define SO_LONG_H
 
-# define WINDOW_WIDTH 600
-# define WINDOW_HEIGHT 300
+# include <fcntl.h>//open, O_RDONLY
+# include <unistd.h>//read, close, write
+# include <stdlib.h>//malloc
+# include <stdio.h>//printf
+# include <stdbool.h>//bool
 
-int main(void)
-{
-    void	*mlx_ptr;
-    void	*win_ptr;
-
-    mlx_ptr = mlx_init();
-    win_ptr = mlx_new_window(mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT, "My first window!");
-    mlx_destroy_window(mlx_ptr, win_ptr);
-    mlx_destroy_display(mlx_ptr);
-    free(mlx_ptr);
-
-	return (0);
-}
+#endif
